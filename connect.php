74c93c5f27ef00df5b187a2ef1cc32e982aa7e2d
GOTETI JAYACHANDRA MOHAN LAKSHMI NARASIMHA MURTHY <63448352+jayachandragoteti@users.noreply.github.com>
2020-10-10 20:29:38 +0530

    commit

--- connect.php
+++ connect.php
@@ -2,6 +2,6 @@
 $server="localhost";
 $user="root";
 $pass="";
-$db="fed";
+$db="sac";
 $conn=mysqli_connect($server,$user,$pass,$db);
 ?>
--- display.php
+++ display.php
@@ -19,21 +19,26 @@
 		</tr>
 		
 		<?PHP
-		include 'connect.php';
-		$query = "SELECT * FROM `back` ";
-		$sql = mysqli_query($conn,$query);
-		if ($sql) 
-		{
-			while($row = mysqli_fetch_array($sql))
-			{
-		       echo "<tr>
-		       <td>".$row['stars']."</td>
-		       <td>".$row['review']."</td>
-		       <td>".$row['summary']."</td>
-		       </tr>";
-    		}
-		}
-			
+			include 'connect.php';
+			$star_query = "SELECT * FROM `back` ";
+			$star_sql =mysqli_query($conn,$star_query);
+			$star_no_row=mysqli_num_rows($star_sql);
+			for ($i=1; $i < 6 ; $i++) { 
+				$star_1_query = "SELECT * FROM `back` WHERE `stars` ='$i'";
+				$star_1_sql =mysqli_query($conn,$star_1_query);
+				if ($star_1_sql) 
+				{
+					$star_1_no_row=mysqli_num_rows($star_1_sql);
+					$start_count_array[$i]=$star_1_no_row;
+				}
+			}
+			$final=0;
+			foreach ($start_count_array as $key => $value){
+				echo  "star ".$key." : ".$value."  => ".($value/$star_no_row)*100  ."%"."<br>";
+				$star_per = ($value/$star_no_row) * 100;
+				$final = $final + $value;
+			}
+				echo "final rating : ".$final/5.;
 		?>
 	
 
